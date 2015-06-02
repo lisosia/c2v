338940710e8838a3a1722ff1b40acb8d331d8c22
lisosia <dev0dsp@gmail.com>
2015-06-02 21:41:53 +0900

    --- ConsensusReader --- bugFix(GT==0/1 (?) の時のバグ) alts_num field 追加 MaleのXY染色体の時の挙動注意 ( 実際のconsensusFileがどうなっているのかわからないので暫定 変異がないとき alt eq "." 変異あるとき alt eq [ACGT] となるとして書いた もし変異があるときaltが A,G など３文字表現であったらどうなるのか..., 挙動未定,実際のconsensusFileで確認してから)

--- src/genome/ConsensusReader.java
+++ src/genome/ConsensusReader.java
@@ -14,8 +14,10 @@ public class ConsensusReader {
 	BufferedReader br;
 	Matcher matcher = null;
 	ConsensusLineInfo lineInfo;
+	Sex sex;
 
-	public ConsensusReader(String filename) throws IOException {
+	public ConsensusReader(String filename, Sex sex) throws IOException {
+		this.sex = sex;
 		InputStream is = FileOpen.openCompressedFile(filename);
 		this.br = new BufferedReader(new InputStreamReader(is));
 	}
@@ -37,7 +39,10 @@ public class ConsensusReader {
 				// TODO いまのところ false を返すのは INDEL の時のみ
 				continue;
 			}
-
+			
+			// 男性のXY染色体の時, bamが 0/1 のものは信頼できない 
+			if(this.sex == Sex.Male && lineInfo.alts_num == 1) {continue;}
+			
 			if (lineInfo.isReliable()) {
 				return true;
 			} else {
@@ -47,21 +52,43 @@ public class ConsensusReader {
 
 		return false;
 	}
-
+	
+	/**
+	 * X染色体についてのみ
+	 * @param pos
+	 * @return
+	 */
+	public static boolean isPAR_X(int pos) {
+		if (((pos > 2699520) && (pos < 154931044)) || (pos <60001 ) || (pos > 155270560)) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+		
+	public static boolean isPAR_Y(int pos) {
+		if(((pos > 2649520) && (pos < 59034050)) || (pos <10001 ) || (pos > 59373566)) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
 	public static class ConsensusLineInfo {
 		// chr pos (id) ref alt qual (filter==DOT) info(DP=?; rarely,INDEL;)
 		// format(pl) BAM[0/0 or ...]num(謎)
 		// TODO bam の形式、その意味が　不明
-		final static Pattern pattern = Pattern.compile("chr(\\d+)\\t" // CHR
-				+ "(\\d+)\\t" // POS
-				+ "\\.\\t" // ID
-				+ "(\\S+)\\t" // REF
-				+ "(\\S+)\\t" // ALT
-				+ "(\\S+)\\t" // QUAL
-				+ "\\.\\t" // FILTER
-				+ "(\\S+)\\t" // INFO
-				+ "(\\S+)\\t" // FORMAT
-				+ "\\S+"); // BAM
+		final static Pattern pattern = Pattern.compile(
+				"chr(\\d+)\\t" //1: CHR
+				+ "(\\d+)\\t" // 2: POS
+				+ "\\.\\t" //       ID
+				+ "(\\S+)\\t" // 3: REF
+				+ "(\\S+)\\t" // 4: ALT
+				+ "(\\S+)\\t" // 5: QUAL
+				+ "\\.\\t"    //    FILTER
+				+ "(\\S+)\\t" // 6: INFO
+				+ "(\\S+)\\t" // 7: FORMAT
+				+ "(\\S+)"); //  8: BAM
 
 		public int chr; // X,Yの時はそれぞれ -1,0 とする
 		public int position;
@@ -69,7 +96,8 @@ public class ConsensusReader {
 		public float qual;
 		public boolean isIndel;
 		public int dp;
-
+		public int alts_num;
+		
 		final private double minimumQual;
 		final private int minimumDP;
 
@@ -78,7 +106,13 @@ public class ConsensusReader {
 			this.minimumQual = minimumQual;
 			this.minimumDP = minimumDP;
 		}
-
+		
+		/**
+		 * line をparseしてlineinfoに格納する. 変異が片方だけの時は altsComparedToRef[0]のほうが変異(非0)
+		 * @param line
+		 * @return 入力が有効な(INDELなどでない)ときはtrue
+		 * @throws IllegalArgumentException 入力のフォーマットが不正の時
+		 */
 		public boolean parseLine(String line) {
 			Matcher m = pattern.matcher(line);
 			if (!m.matches()) {
@@ -92,22 +126,23 @@ public class ConsensusReader {
 			this.qual = Float.parseFloat(m.group(5));
 			String info = m.group(6);
 			// TODO bam(最後の) なにに使うの???
-			String bam = m.group(7);
+			String bam = m.group(8);
+			
+			this.isIndel = false;
 			if (info.contains("INDEL")) {
+				this.isIndel = true;
 				return false;
-			} // INDEL 即リターン. この行を変えるときは, 後ろでバグらないよう注意
+			} // INDEL あったら即リターン. この行を変えるときは, 後ろでバグらないよう注意
 
 			// set altsComparedToRef
-			if (alts.equals(".")) {
+			if (alts.equals(".")) { // 変異なし
 				altsComparedToRef[0] = altsComparedToRef[1] = 0;
-			} else if (alts.length() == 1) {
-				altsComparedToRef[0] = altsComparedToRef[1] = ParseBase
-						.returnDiff(ref, alts);
+			} else if (alts.length() == 1) { // alts == [ACGT] and different from ref(ACGT)
+				altsComparedToRef[0] = ParseBase.returnDiff(ref, alts);
+				altsComparedToRef[1] = 0;
 			} else if (alts.length() == 3) {
-				altsComparedToRef[0] = ParseBase.returnDiff(ref,
-						alts.substring(0, 1));
-				altsComparedToRef[1] = ParseBase.returnDiff(ref,
-						alts.substring(2, 3));
+				altsComparedToRef[0] = ParseBase.returnDiff(ref,alts.substring(0, 1));
+				altsComparedToRef[1] = ParseBase.returnDiff(ref,alts.substring(2, 3));
 			} else {
 				// NEVER Reach HERE if consensus is correct format
 				throw new IllegalArgumentException("patternMatch failed. " + 
@@ -126,10 +161,22 @@ public class ConsensusReader {
 						"info is following:\n" + info);
 			}
 			
+			//set alts_num
+			if(bam.equals("0")){
+				this.alts_num = 0;				
+			}else if(bam.startsWith("0/1") || bam.startsWith("1/0")){
+				this.alts_num = 1;
+			}else if( bam.startsWith("1/1") ){
+				this.alts_num = 2;
+			}else {
+				throw new IllegalArgumentException("Unexpected bam file format" + 
+						"consensus line:\n" + line);
+			}
+			
 			return true;
 		}
 
-		boolean isReliable() {
+		public boolean isReliable() {
 			if (!this.isIndel && qual >= this.minimumQual
 					&& dp >= this.minimumDP) {
 				return true;
